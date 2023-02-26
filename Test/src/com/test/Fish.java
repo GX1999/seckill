a8747e88c43e87a1e06a1891ddc99c27c3a1ee34
Xiang <65434948+GX1999@users.noreply.github.com>
2023-02-26 17:23:36 +0800

    Delete Test directory

--- Test/src/com/test/Fish.java
+++
@@ -1,9 +0,0 @@
-package com.test;
-
-public class Fish {
-    public void divide() throws Exception {
-        int x = 1;
-        int y = 0;
-        int re = x / y;
-    }
-}
--- Test/src/com/test/Pet.java
+++
@@ -1,7 +0,0 @@
-package com.test;
-
-public interface Pet {
-    public int nums = 10;
-
-    public int comparePet();
-}
